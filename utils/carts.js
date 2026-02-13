269c1a8f49a0bf68f045f9f39f34c7fa4fd3018c
BexruzBexruz202 <vayron567@gmail.com>
2026-02-13 15:56:30 +0300

    admin_panel

--- utils/carts.js
+++ utils/carts.js
@@ -1,76 +1,23 @@
-const API = "https://fakestoreapi.com/carts";
-const cartList = document.getElementById("cartList");
-const modal = document.getElementById("modal");
-const userIdInput = document.getElementById("userId");
-const dateInput = document.getElementById("date");
-let editId = null;
+const API = "https://fakestoreapi.com/carts"
+const cartList = document.getElementById("cartList")
+const modal = document.getElementById("modal")
+const addBtn = document.getElementById("addCartBtn")
+const closeBtn = document.getElementById("close")
 
-document.querySelector(".logout__btn").onclick = () => {
-  localStorage.clear();
-  location.href = "/pages/index.html";
-};
+axios.get(API).then(r => {
+  cartList.innerHTML = ""
+  r.data.forEach(c => {
+    cartList.innerHTML += `
+      <tr>
+        <td>${c.id}</td>
+        <td>${c.userId}</td>
+        <td>${c.date.split("T")[0]}</td>
+        <td>${c.products.length}</td>
+        <td><button class="edit">View</button></td>
+      </tr>
+    `
+  })
+})
 
-function fetchCarts() {
-  axios.get(API).then(({ data }) => {
-    cartList.innerHTML = "";
-
-    data.slice(0, 10).forEach((cart) => {
-      const tr = document.createElement("tr");
-
-      tr.innerHTML = `
-        <td>${cart.id}</td>
-        <td>${cart.userId}</td>
-        <td>${new Date(cart.date).toLocaleDateString()}</td>
-        <td>${cart.products.length}</td>
-        <td class="actions">
-          <button class="edit">Edit</button>
-          <button class="delete">Delete</button>
-        </td>
-      `;
-
-      tr.querySelector(".edit").addEventListener("click", () => editCart(cart));
-
-      tr.querySelector(".delete").addEventListener("click", () =>
-        deleteCart(cart.id),
-      );
-
-      cartList.appendChild(tr);
-    });
-  });
-}
-
-fetchCarts();
-
-function editCart(cart) {
-  modal.classList.remove("hidden");
-  editId = cart.id;
-
-  userIdInput.value = cart.userId;
-  dateInput.value = cart.date;
-  productCountInput.value = cart.products.length;
-}
-
-document.getElementById("saveCart").onclick = () => {
-  const count = Number(productCountInput.value);
-
-  const updatedCart = {
-    userId: Number(userIdInput.value),
-    date: dateInput.value,
-    products: Array.from({ length: count }, (_, i) => ({
-      productId: i + 1,
-      quantity: 1,
-    })),
-  };
-
-  axios.put(`${API}/${editId}`, updatedCart).then(() => {
-    modal.classList.add("hidden");
-    fetchCarts();
-  });
-};
-function deleteCart(id) {
-  axios.delete(`${API}/${id}`).then(fetchCarts);
-}
-
-document.getElementById("close").onclick = () => {
-  modal.classList.add("hidden");
-};
+addBtn.onclick = () => modal.classList.remove("hidden")
+closeBtn.onclick = () => modal.classList.add("hidden")
--- utils/users.js
+++ utils/users.js
@@ -1,31 +1,16 @@
-const elUsers = document.querySelector("#users");
-const elLogout = document.querySelector(".logout");
+const API = "https://fakestoreapi.com/users"
+const list = document.getElementById("productList")
 
-fetch("https://fakestoreapi.com/users")
-  .then(res => res.json())
-  .then(users => renderUsers(users))
-  .catch(err => console.error(err));
-
-function renderUsers(users) {
-  elUsers.innerHTML = "";
-
-  users.forEach(user => {
-    const div = document.createElement("div");
-    div.className = "user";
-
-    div.innerHTML = `
-      <h3 class = "users_text">${user.name.firstname} ${user.name.lastname}</h3>
-      <p class = "users_text"><b>Username:</b> ${user.username}</p>
-      <p class = "users_text"><b>Email:</b> ${user.email}</p>
-      <p class = "users_text"><b>Phone:</b> ${user.phone}</p>
-      <p class = "users_text"><b>City:</b> ${user.address.city}</p>
-    `;
-
-    elUsers.appendChild(div);
-  });
-}
-
-elLogout.addEventListener("click", () => {
-  localStorage.removeItem("token");
-  window.location.href = "../index.html";
-});
+axios.get(API).then(r => {
+  list.innerHTML = ""
+  r.data.forEach(u => {
+    list.innerHTML += `
+      <tr>
+        <td>${u.id}</td>
+        <td>${u.username}</td>
+        <td>${u.email}</td>
+        <td>${u.phone}</td>
+      </tr>
+    `
+  })
+})
